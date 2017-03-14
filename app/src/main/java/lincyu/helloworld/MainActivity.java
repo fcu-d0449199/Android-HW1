6e003d13717cda74d4140eff84a3275f0963cdf9
MSI\小青蛙1 <wendy92433@gmail.com>
2017-03-15 01:14:54 +0800

    first comment

---
+++ app/src/main/java/lincyu/helloworld/MainActivity.java
@@ -0,0 +1,34 @@
+package lincyu.helloworld;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button nextPageBtn = (Button)findViewById(R.id.button);
+        nextPageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this , Page2.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void buttonOnClick(View view) {
+        Toast toast = Toast.makeText(this, "按鈕已經被點擊", Toast.LENGTH_SHORT);
+        toast.show();
+    }
+}
---
+++ app/src/main/java/lincyu/helloworld/Page2.java
@@ -0,0 +1,16 @@
+package lincyu.helloworld;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class Page2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
